0d3f0860701cea84e77304de6e6ad91cc047536b
ondieki1237 <makoriseth1237@gmail.com>
2025-10-30 06:04:18 +0300

    testing1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",                # local dev
-        "https://your-frontend-prod.com",       # deployed frontend
+        "https://your-frontend-prod.com",       
     ],
     allow_credentials=True,
     allow_methods=["*"],
